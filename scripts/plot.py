2e491db9165d8def901855f48f9ff580b40cd16e
Gerd Zellweger <mail@gerdzellweger.com>
2020-03-23 15:41:39 -0700

    Update run scripts to run different experiments and size.
    Also change plot script to rely on CSV format of output.

--- scripts/plot.py
+++ scripts/plot.py
@@ -4,7 +4,7 @@
 Script that plots benchmark data-visualizations.
 """
 
-import sys
+import sys, os
 import pandas as pd
 import numpy as np
 import plotnine as p9
@@ -12,6 +12,7 @@ import re
 
 from plotnine import *
 from plotnine.data import *
+import humanfriendly
 
 import warnings
 
@@ -41,78 +42,28 @@ class theme_my538(theme_gray):
                 strip_background=element_rect(size=0)),
             inplace=True)
 
-def plot_scalability(df):
+def plot_scalability(filename, df):
     "Plots a throughput graph for various threads showing the throughput over time"
-    benchmark = df.groupby(['ncores', 'config'], as_index=False).agg({'tput': 'sum', 'tid': 'count', 'runtime': 'max'})
+    benchmark = df.groupby(['ncores', 'benchmark', 'memsize'], as_index=False).agg({'operations': 'sum', 'thread_id': 'count', 'duration': 'max'})
     MS_TO_SEC = 0.001
-    benchmark['tps'] = benchmark['tput'] / (benchmark['runtime'] * MS_TO_SEC)
-    print(benchmark)
+    benchmark['tps'] = (benchmark['operations'] / (benchmark['duration'] * MS_TO_SEC)).fillna(0.0).astype(int)
+    benchmark['memsize_fmt'] = benchmark['memsize'].transform(lambda val: humanfriendly.format_size(val, binary=True))
 
-    p = ggplot(data=benchmark, mapping=aes(x='ncores', y='tps', ymin=0, xmax=192, color='config')) + \
+    print(benchmark)
+    p = ggplot(data=benchmark, mapping=aes(x='ncores', y='tps', ymin=0, xmax=192, color='memsize_fmt', group='memsize_fmt')) + \
         theme_my538(base_size=13) + \
         labs(y="Throughput [Kelems/s]", x = "# Threads") + \
         theme(legend_position='top', legend_title = element_blank()) + \
-        scale_y_log10(labels=lambda lst: ["{:,}".format(x / 100_000) for x in lst]) + \
+        scale_y_log10(labels=lambda lst: ["{:,.2f}".format(x / 100_000) for x in lst]) + \
         geom_point() + \
         geom_line()
 
-    p.save("throughput.png", dpi=300)
-    p.save("throughput.pdf", dpi=300)
+    p.save("{}-throughput.png".format(filename), dpi=300)
+    p.save("{}-throughput.pdf".format(filename), dpi=300)
 
 
 def parse_results(path):
-    """
-    Parse output that looks like this in  a file:
-    
-    + VMOPS Benchmark
-    + VMOPS Selecting the independent configuration.
-    thread 0 started. Running for 20000 ms.
-    thread 0 ended. 18382031 map + unmaps
-    + VMOPS Benchmark done. ncores=1, total ops = 18382031
-
-    Returns a DataFrame that looks like this:
-    ncores, tid, tput
-    1, 0, 18382031
-    """
-    
-    benchmark_idx = 1
-    df = pd.DataFrame()
-    thread_regex = re.compile('thread (\d+) ended\. (\d+) .*')
-    thread_start_regex = re.compile('thread (\d+) started\. Running for (\d+) .*')
-    benchmark_done_regex = re.compile('\+ VMOPS Benchmark done\. ncores=(\d+), total ops = (\d+)')
-    threads_times = {}
-    threads_tput = {}
-    results = []
-    configuration = "unknown"
-
-    with open(path) as f:
-        for line in f.readlines():
-            if "+ VMOPS Selecting the independent configuration." in line:
-                configuration = "independent"
-            if line == "+ VMOPS Benchmark":
-                benchmark_idx += 1
-                threads_tput = {}
-                threads_times = {}
-            if "thread" in line and "started" in line:
-                # parse "thread 0 started. Running for 20000 ms."
-                thread_id, thread_time = thread_start_regex.match(line).groups()
-                threads_times[int(thread_id)] = int(thread_time)
-            if "thread" in line and "ended" in line:
-                # parse "thread 0 ended. 18382031 map + unmaps"
-                thread_id, thread_tput = thread_regex.match(line).groups()
-                threads_tput[int(thread_id)] = int(thread_tput)
-            if "VMOPS Benchmark done." in line and "ncores" in line:
-                # parse: VMOPS Benchmark done. ncores=1, total ops = 18382031
-                ncores, total_ops = benchmark_done_regex.match(line).groups()
-                ncores = int(ncores)
-                total_ops = int(total_ops)
-                assert total_ops == sum(threads_tput.values())
-                # thread times should all be the same:
-                assert list(threads_times.values())[0]*len(threads_times.values()) == sum(threads_times.values())
-
-                for tid, tput in threads_tput.items():
-                    results.append([configuration, ncores, tid, tput, threads_times[tid]])
-    return pd.DataFrame(results, columns=['config', 'ncores', 'tid', 'tput', 'runtime'])
+    return pd.read_csv(path)
 
 if __name__ == '__main__':
     warnings.filterwarnings('ignore')
@@ -127,5 +78,5 @@ if __name__ == '__main__':
 
     df = parse_results(sys.argv[1])
     print(df)
-    plot_scalability(df)
+    plot_scalability(os.path.basename(sys.argv[1]), df)
     
--- scripts/run.sh
+++ scripts/run.sh
@@ -7,18 +7,23 @@ DURATION_MS=20000
 MAX_CORES=`nproc`
 
 declare -a benchmarks=("mapunmap-isolated" "mapunmap-independent" "mapunmap-shared-isolated" "mapunmap-shared-independent" "mapunmap-4k-isolated" "mapunmap-4k-independent" "protect-shared" "protect-independent" "protect-4k-independent")
+declare -a memsizes=("4096" "2097152")
+
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+rm *.log *.csv /dev/shm/vmops_bench_* || true
 
 for benchmark in "${benchmarks[@]}"; do
-    LOGFILE=${HOSTNAME}_results_${benchmark}.log
-    CSVFILE=${HOSTNAME}_results_${benchmark}.csv
-    echo "thread_id,benchmark,core,ncores,memsize,duration,operations" | tee -a $CSVFILE
-    if test -f "$LOGFILE"; then
-        echo "$LOGFILE with previous results already exists."
-        exit 1
-    fi
+    for memsize in "${memsizes[@]}"; do
 
-    for cores in `seq 0 4 $MAX_CORES`; do
-        cat /proc/interrupts | grep TLB | tee -a $LOGFILE;
-        (./bin/vmops -p $cores -t $DURATION_MS -b ${benchmark} | tee -a $CSVFILE) 3>&1 1>&2 2>&3 | tee -a $LOGFILE
+        LOGFILE=${HOSTNAME}_results_${benchmark}.log
+        CSVFILE=${HOSTNAME}_results_${benchmark}.csv
+        if [ ! -f "$CSVFILE" ]; then
+            echo "thread_id,benchmark,core,ncores,memsize,duration,operations" | tee $CSVFILE
+        fi
+
+        for cores in `seq 0 4 $MAX_CORES`; do
+            cat /proc/interrupts | grep TLB | tee -a $LOGFILE;
+            (./bin/vmops -p $cores -t $DURATION_MS -m $memsize -b ${benchmark} | tee -a $CSVFILE) 3>&1 1>&2 2>&3 | tee -a $LOGFILE
+        done
     done
 done
