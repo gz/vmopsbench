d735e8346c59b3e98421561e828bb1dacf25b79c
Gerd Zellweger <mail@gerdzellweger.com>
2020-04-05 12:36:54 -0700

    Update run script
    Include all configs.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,27 +3,44 @@ set -ex
 make -j 12
 
 HOSTNAME=`hostname`
-DURATION_MS=20000
+DURATION_MS=10000
 MAX_CORES=`nproc`
 
-declare -a benchmarks=("mapunmap-isolated" "mapunmap-independent" "mapunmap-shared-isolated" "mapunmap-shared-independent" "mapunmap-4k-isolated" "mapunmap-4k-independent" "protect-shared" "protect-independent" "protect-4k-independent")
+declare -a bench=("mapunmap" "maponly" "protect")
+declare -a isolation=("" "-isolated")
+declare -a sharing=("independent" "shared")
+declare -a mappings=("" "-4k")
+
 declare -a memsizes=("4096" "2097152")
+declare -a numa=("" "-i")
 
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-rm *.log *.csv /dev/shm/vmops_bench_* || true
+#echo never > /sys/kernel/mm/transparent_hugepage/enabled
+rm *.log *.csv *.png *.pdf /dev/shm/vmops_bench_* || true
+sudo sysctl -w vm.max_map_count=50000000
 
-for benchmark in "${benchmarks[@]}"; do
-    LOGFILE=${HOSTNAME}_results_${benchmark}.log
-    CSVFILE=${HOSTNAME}_results_${benchmark}.csv
-    for memsize in "${memsizes[@]}"; do
-        if [ ! -f "$CSVFILE" ]; then
-            echo "thread_id,benchmark,core,ncores,memsize,duration,operations" | tee $CSVFILE
-        fi
+for b in "${bench[@]}"; do
+    for i in "${isolation[@]}"; do
+        for s in "${sharing[@]}"; do
+            for m in "${mappings[@]}"; do
+                benchmark="$b$i-$s$m"
+                for numa in "${numa[@]}"; do
+                    LOGFILE=${HOSTNAME}_results_${benchmark}${numa}.log
+                    CSVFILE=${HOSTNAME}_results_${benchmark}${numa}.csv
 
-        for cores in `seq 0 4 $MAX_CORES`; do
-            cat /proc/interrupts | grep TLB | tee -a $LOGFILE;
-            (./bin/vmops -p $cores -t $DURATION_MS -m $memsize -b ${benchmark} | tee -a $CSVFILE) 3>&1 1>&2 2>&3 | tee -a $LOGFILE
+                    for memsize in "${memsizes[@]}"; do
+                        if [ ! -f "$CSVFILE" ]; then
+                            echo "thread_id,benchmark,core,ncores,memsize,duration,operations" | tee $CSVFILE
+                        fi
+
+                        for cores in `seq 0 4 $MAX_CORES`; do
+                            cat /proc/interrupts | grep TLB | tee -a $LOGFILE;
+                            (./bin/vmops -p $cores -t $DURATION_MS -m $memsize -b ${benchmark} ${numa} | tee -a $CSVFILE) 3>&1 1>&2 2>&3 | tee -a $LOGFILE
+                        done
+                    done
+
+                    python3 ./scripts/plot.py ${CSVFILE}
+                done
+            done
         done
     done
-    python3 ./scripts/plot.py ${CSVFILE}
 done
