224b06578be194370e4663296ac31452ce7256c0
Gerd Zellweger <mail@gerdzellweger.com>
2020-04-22 14:34:49 -0700

    Fix ci script.

--- scripts/ci.bash
+++ scripts/ci.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex 
 
+pip3 install -r requirements.txt
+
 make clean
 make
 
@@ -11,9 +13,8 @@ make profile-maponly-isolated-4
 
 rm *.log *.csv *.png *.pdf /dev/shm/vmops_bench_* || true
 sudo sysctl -w vm.max_map_count=50000000
-echo 10 | sudo tee  /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
+echo 192 | sudo tee  /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 
-HOSTNAME=`hostname`
 DURATION_MS=10000
 MAX_CORES=`nproc`
 
@@ -22,8 +23,8 @@ numa=''
 huge=''
 memsize='4096'
 
-LOGFILE=${HOSTNAME}_results_${benchmark}.log
-CSVFILE=${HOSTNAME}_results_${benchmark}.csv
+LOGFILE=results_${benchmark}.log
+CSVFILE=results_${benchmark}.csv
 
 echo "thread_id,benchmark,core,ncores,memsize,numainterleave,mappings_size,page_size,memobj,isolation,duration,operations" | tee $CSVFILE
 for cores in `seq 0 8 $MAX_CORES`; do
@@ -33,8 +34,33 @@ done
 python3 scripts/plot.py $CSVFILE
 
 python3 scripts/run_barrelfish.py --cores 1 --verbose --hake
-python3 scripts/run_barrelfish.py --cores 1,2
-python3 scripts/run_barrelfish.py --cores 1,2,3
-python3 scripts/run_barrelfish.py --cores 1,2,3,4
+python3 scripts/run_barrelfish.py --cores 1 2
+python3 scripts/run_barrelfish.py --cores 1 2 3
+python3 scripts/run_barrelfish.py --cores 1 2 3 4
 
+rm -rf gh-pages
 git clone -b gh-pages git@github.com:gz/vmops-bench.git gh-pages
+CI_MACHINE_TYPE="ryzen5"
+
+export GIT_REV_CURRENT=`git rev-parse --short HEAD`
+export CSV_LINE="`date +%Y-%m-%d`",$GIT_REV_CURRENT,"vmops/$GIT_REV_CURRENT/index.html"
+echo $CSV_LINE >> gh-pages/_data/$CI_MACHINE_TYPE.csv
+
+DEPLOY_DIR="gh-pages/${CI_MACHINE_TYPE}/vmops/${GIT_REV_CURRENT}/"
+rm -rf ${DEPLOY_DIR}
+mkdir ${DEPLOY_DIR}
+cp gh-pages/vmops/index.markdown ${DEPLOY_DIR}
+
+mv barrelfish_results.csv ${DEPLOY_DIR}
+mv *.log ${DEPLOY_DIR}
+mv *.csv ${DEPLOY_DIR}
+mv *.pdf ${DEPLOY_DIR}
+mv *.png ${DEPLOY_DIR}
+
+cd gh-pages
+git add .
+git commit -a -m "Added benchmark results for $GIT_REV_CURRENT."
+git push origin gh-pages
+cd ..
+rm -rf gh-pages
+git clean -f
