40b0e43c85434401125266a357968e12c8fccdfe
Ankit Bhardwaj <ankit-iitb@users.noreply.github.com>
2020-05-09 01:13:06 -0600

    Avoid failing at umount.

--- scripts/ci.bash
+++ scripts/ci.bash
@@ -4,7 +4,7 @@ set -ex
 pip3 install -r scripts/requirements.txt
 
 rm *.log *.csv *.png *.pdf /dev/shm/vmops_bench_* || true
-sudo umount -f /mnt
+sudo umount -f /mnt || true
 # Mount tmpfs
 sudo mount tmpfs /mnt -t tmpfs -o size=50G
 
